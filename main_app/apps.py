9d4122fb48299f4da53a38d74a6ea0df8c16c54e
Xiao Zhong <zhongxiao2017@gmail.com>
2023-12-20 15:10:21 +1100

    deploying

--- main_app/apps.py
+++ main_app/apps.py
@@ -5,6 +5,6 @@ class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
 
-    def ready(self):
-        from . import initialization
-        initialization.create_user_groups()
+    # def ready(self):
+    #     from . import initialization
+    #     initialization.create_user_groups()
